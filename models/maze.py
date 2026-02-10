0c6865fef7e59c2a6f69cab6e215661c950ebba9
Youssef Ouahbi <wahbi0659@gmail.com>
2026-02-10 13:47:54 +0100

    files

--- models/maze.py
+++ models/maze.py
@@ -24,7 +24,34 @@ class Maze():
             return self.grid[row][col]
 
     def get_neighbors(self, cell, visited_only=False):
-        pass
+        neighbors = []
+        r = cell.row
+        c = cell.col
+
+        # North
+        north = self.get_cell(r - 1, c)
+        if north is not None:
+            if not visited_only or not north.visited:
+                neighbors.append((north, "north"))
+
+        # South
+        south = self.get_cell(r + 1, c)
+        if south is not None:
+            if not visited_only or not south.visited:
+                neighbors.append((south, "south"))
+
+        # East
+        east = self.get_cell(r, c + 1)
+        if east is not None:
+            if not visited_only or not east.visited:
+                neighbors.append((east, "east"))
+
+        # West
+        west = self.get_cell(r, c - 1)
+        if west is not None:
+            if not visited_only or not west.visited:
+                neighbors.append((west, "west"))
+        return neighbors
 
     def reset_visited(self):
         pass
